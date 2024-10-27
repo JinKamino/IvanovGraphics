1d19f4a34695dc11229febea57dec70b76da596b
Harley <92978645+BeidinaDaria@users.noreply.github.com>
2024-10-27 18:30:23 +0300

    Update polyhedron-script.js

--- lab6/polyhedron-script.js
+++ lab6/polyhedron-script.js
@@ -8,12 +8,14 @@ class Polyhedron {
     }
 
     reflect(axis) {
+        const c = 0;//90
+        const s = 1;
+        let matrix = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+        if (axis === 'x') matrix = [[1, 0, 0], [0, c, -s], [0, s, c]];
+        else if (axis === 'y') matrix = [[c, 0, s], [0, 1, 0], [-s, 0, c]];
+        else if (axis === 'z') matrix = [[c, -s, 0], [s, c, 0], [0, 0, 1]];
         this.polygons.forEach(polygon => {
-            polygon.vertices.forEach(vertex => {
-                if (axis === 'x') vertex.coordinates[0] = -vertex.coordinates[0];
-                else if (axis === 'y') vertex.coordinates[1] = -vertex.coordinates[1];
-                else if (axis === 'z') vertex.coordinates[2] = -vertex.coordinates[2];
-            });
+            polygon.vertices=this.multiplyMatrices(polygon.vertices,matrix);
         });
     }
 
@@ -35,5 +37,27 @@ class Polyhedron {
             return acc;
         }, [0, 0, 0]);
         return sum.map(coord => coord / allVertices.length);
+    }
+
+    multiplyMatrices(vertices, matrix) {
+        // Проверка совместимости размеров матриц
+        const result = [];
+        let buf;
+        for (let i = 0; i < vertices.length; i++) {
+            buf=[];
+    
+            for (let j = 0; j < vertices[0].coordinates.length; j++) {
+                let sum = 0;
+    
+                for (let k = 0; k < matrix[0].length; k++) {
+                    sum += vertices[i].coordinates[k] * matrix[k][j];
+                }
+    
+                buf[j] = sum;
+            }
+            result[i]=new Point(buf[0],buf[1],buf[2]);
+        }
+    
+        return result;
     }
 }
